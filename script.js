f5fec299e2cf3bd26a96cd4f3b5daa75a94890cf
Jeffrey Lee <jglee1@live.com>
2020-11-18 23:40:43 -0800

    Added a loading animation

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const newQuoteBtn = document.getElementById('new-quote');
 const twitterBtn = document.getElementById('twitter');
+const loader = document.getElementById('loader');
+
+//Show Loader
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//Hide loader
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
 //Get Quote from API
 async function getQuote() {
+  loading();
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
@@ -26,6 +42,8 @@ async function getQuote() {
       quoteText.classList.remove('long-quote');
     }
     quoteText.innerText = data.quoteText;
+    //Stop loader and show quote
+    complete();
   } catch (error) {
     getQuote()
     console.log('no quote', error)
