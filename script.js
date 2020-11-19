c845aacef9b82d0e7cd7cb33c2dba2736047a210
Jeffrey Lee <jglee1@live.com>
2020-11-18 23:33:16 -0800

    Functioning buttons completed

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const newQuoteBtn = document.getElementById('new-quote');
+const twitterBtn = document.getElementById('twitter');
 
 //Get Quote from API
 async function getQuote() {
@@ -18,7 +19,7 @@ async function getQuote() {
       authorText.innerText = data.quoteAuthor;
     }
     //Reduce font size for long quotes
-    if (data.quoteText.length > 50) {
+    if (data.quoteText.length < 120) {
       quoteText.classList.add('long-quote');
     }
     else {
@@ -30,6 +31,18 @@ async function getQuote() {
     console.log('no quote', error)
   }
 }
+
+//function for twitter button to tweet quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
+
+//event listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 //On Load
 getQuote()
